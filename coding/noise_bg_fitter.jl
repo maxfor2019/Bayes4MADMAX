89828550ab910df3008d44aecbb75220b909f012
Jakob Knollmüller <jakobknollmuller@ext037.eduroam.mppmu.mpg.de>
2021-09-10 15:42:19 +0200

    add files

---
+++ coding/noise_bg_fitter.jl
@@ -0,0 +1,197 @@
+using BAT
+using Random, LinearAlgebra
+using Distributions
+using ValueShapes
+using Plots
+using FFTW
+import ForwardDiff
+using DelimitedFiles
+using PDMats
+using MGVI
+using Optim
+# using Zygote: @adjoint, @ignore, gradient
+using Zygote
+data = readdlm("./data/Fake_Axion_Data/Data_Set_1/Test00Osc01_17-01-24_0915.dat")#, '\t', Float32, '\n')
+
+
+vals = data[11001:11100,2]
+vals = (vals .- mean(vals))./std(vals)
+dists = (data[:,1] - circshift(data[:,1],1))
+# vals = vals[12001:12100]
+
+
+
+plot(data[:,1],data[:,2], ylims=(minimum(vals),maximum(vals)))
+# data = data[1:100]
+data = vals
+# distances = (mean(dists[2:end]),)
+# distances = (1/100,)
+dims = (length(data[:,1]),)
+distances = (1/dims[1],)
+
+harmonic_pad_distances = 1 ./ (2 .* dims .* distances)
+
+
+
+function map_idx(idx::Real, idx_range::AbstractUnitRange{<:Integer})
+    i = idx - minimum(idx_range)
+    n = length(eachindex(idx_range))
+    n_2 = n >> 1
+    ifelse(i <= n_2, i, i - n)
+end
+
+function dist_k(idx::CartesianIndex, ax::NTuple{N,<:AbstractUnitRange{<:Integer}}, harmonic_distances::NTuple{N,<:Real}) where N
+    mapped_idx = map(map_idx, Tuple(idx), ax)
+    norm(map(*, mapped_idx, harmonic_distances))
+end
+
+function dist_array(dims::NTuple{N,<:Real}, harmonic_distances::NTuple{N,<:Real}) where N
+    cart_idxs = CartesianIndices(map(Base.OneTo, dims))
+    dist_k.(cart_idxs, Ref(axes(cart_idxs)), Ref(harmonic_distances))
+end
+
+
+function amplitude_spectrum(d::Real, zero_mode_dist::Normal, slope::Real, offset::Real)
+    # R = float(promote_type(typeof(d), typeof(zero_mode), typeof(slope), typeof(offset)))
+    # d ≈ 0 : R(zero_mode), R(exp(offset + slope * log(d)))
+    ifelse(d ≈ 0, promote(std(zero_mode_dist), exp(offset + slope * log(d)))...)
+end
+
+
+function my_mask(x)
+    #  y = x[mask]
+     y = x[1:length(data)] #FIXME
+     y
+end
+
+# function adjoint_mask(x)
+#     y = zeros(size(mask))
+#     y[mask] = x
+#     return y
+# end
+
+# @adjoint my_mask(x) = my_mask(x), y -> (adjoint_mask(y),)
+
+
+
+
+# function adjoint_mask(dp::Vector{ForwardDiff.Dual{T, V, N}}) where {T,V,N}
+#     val_res = adjoint_mask(ForwardDiff.value.(dp))
+#     psize = size(ForwardDiff.partials(dp[1]), 1)
+#     ps = x -> ForwardDiff.partials.(dp, x)
+#     val_ps = map((x -> adjoint_mask(ps(x))), 1:psize)
+#     ForwardDiff.Dual{T}.(val_res, val_ps...)
+# end
+
+
+function amplitude_forward_model(parameters)
+    corr = (parameters.fluctuations ./(1 .+ (D./0.1).^(-parameters.slope)))[2:end]
+    corr = vcat(parameters.zero_mode, corr)
+    # corr[1] = parameters.zero_mode
+    corr.^0.5 
+end
+
+
+function gp_forward_model(parameters)
+    # amplitude = amplitude_forward_model(parameters)
+    amplitude = 1.
+    harmonic_gp = amplitude .* parameters.ξ
+    gp = my_ht(harmonic_gp) * (harmonic_pad_distances[1] / sqrt(length(x_pad)))
+    my_mask(gp)
+end
+
+function my_ht(dp::Vector{Float64})
+    ht * dp
+end
+
+
+function my_ht(dp::Vector{ForwardDiff.Dual{T, V, N}}) where {T,V,N}
+    val_res = ht *  ForwardDiff.value.(dp)
+    psize = size(ForwardDiff.partials(dp[1]), 1)
+    ps = x -> ForwardDiff.partials.(dp, x)
+    val_ps = map((x -> ht*ps(x)), 1:psize)
+    ForwardDiff.Dual{T}.(val_res, val_ps...)
+end
+
+
+rng = Random.default_rng()
+
+# trafo_dht = FFTW.plan_r2r(zeros(2 .* dims), FFTW.DHT)
+x = collect(distances[1]:distances[1]:distances[1] * dims[1];)
+x_pad = collect(distances[1]:distances[1]:distances[1] * dims[1]*2;)
+
+ht = FFTW.plan_r2r(zeros(length(x_pad)), FFTW.DHT);
+D = dist_array(2 .* dims, harmonic_pad_distances)
+
+
+mask = collect(1:length(x_pad))
+mask = mask .<= length(mask)÷2
+
+prior = NamedTupleDist(
+    ξ = BAT.StandardMvNormal(length(D)), 
+    # fluctuations = Uniform(0.1, 90),
+    # zero_mode = Uniform(0.1, 200),
+    # slope = Uniform(-6, -2),
+    n = Uniform(1e-4,10)
+)
+
+truth = rand(prior)
+# data = gp_forward_model(truth)[mask]
+data = vals
+# likelihood = let data = data, f = gp_forward_model
+#     params -> begin
+#         residual = data .- f(params)[mask]
+#         noise_weigted_residual = residual ./ 1.0e-4  #FIXME weights
+#         llhd = - 0.5 .* sum(residual .* noise_weigted_residual)
+#         # llhd = logpdf(MvNormal(μ,N),data)
+#         return LogDVal(llhd)
+#     end
+# end
+
+bwd_trafo = BAT.DistributionTransform(Normal, prior)
+standard_truth = bwd_trafo(truth)
+
+fwd_trafo = inv(bwd_trafo)
+
+standard_prior = BAT.StandardMvNormal(length(standard_truth))
+function model(stand_pars)
+    parameters = fwd_trafo(stand_pars)[1]
+    Product(Normal.(gp_forward_model(parameters), parameters.n))
+
+end
+
+
+
+# sampler =  MCMCSampling(mcalg = HamiltonianMC(), nsteps = 10^4, nchains = 4)
+# r = bat_sample(posterior, sampler)
+
+
+
+
+starting_point = bwd_trafo(rand(prior))
+first_iteration = mgvi_kl_optimize_step(rng,
+                                        model, data,
+                                        starting_point;
+                                        # jacobian_func=FullJacobianFunc,   
+                                        # jacobian_func=FwdDerJacobianFunc,                                        
+                                        jacobian_func=FwdRevADJacobianFunc,
+                                        residual_sampler=ImplicitResidualSampler,
+                                        optim_options=Optim.Options(iterations=10, show_trace=true),
+                                        residual_sampler_options=(;cg_params=(;maxiter=10)))
+
+
+likelihood = x -> LogDVal(MGVI.posterior_loglike(model, x, data))
+posterior = PosteriorDensity(likelihood, standard_prior)
+
+findmode_result = bat_findmode(posterior,MaxDensityLBFGS()).result
+
+max_posterior = Optim.optimize(x -> -MGVI.posterior_loglike(model, x, data),
+                 starting_point, LBFGS(), Optim.Options(show_trace=false, g_tol=1E-10, iterations=300));
+
+plot(data)
+plot!(gp_forward_model(fwd_trafo(Optim.minimizer(max_posterior))[1]))
+plot!(gp_forward_model(fwd_trafo(findmode_result)[1]))
+
+
+
+
--- coding/shapefinder.jl
+++ coding/shapefinder.jl
@@ -1,62 +1,114 @@
 using BAT
-using Random
+using Random, LinearAlgebra
 using Distributions
 using ValueShapes
 using Plots
 using FFTW
 using ForwardDiff
+using PDMats
 
-x = collect(0.01:0.01:1;)
+
+distances = (1/100,)
+dims = (100,)
+harmonic_pad_distances = 1 ./ (2 .* dims .* distances)
+
+
+
+function map_idx(idx::Real, idx_range::AbstractUnitRange{<:Integer})
+    i = idx - minimum(idx_range)
+    n = length(eachindex(idx_range))
+    n_2 = n >> 1
+    ifelse(i <= n_2, i, i - n)
+end
+
+function dist_k(idx::CartesianIndex, ax::NTuple{N,<:AbstractUnitRange{<:Integer}}, harmonic_distances::NTuple{N,<:Real}) where N
+    mapped_idx = map(map_idx, Tuple(idx), ax)
+    norm(map(*, mapped_idx, harmonic_distances))
+end
+
+function dist_array(dims::NTuple{N,<:Real}, harmonic_distances::NTuple{N,<:Real}) where N
+    cart_idxs = CartesianIndices(map(Base.OneTo, dims))
+    dist_k.(cart_idxs, Ref(axes(cart_idxs)), Ref(harmonic_distances))
+end
+
+
+function amplitude_spectrum(d::Real, zero_mode_dist::Normal, slope::Real, offset::Real)
+    # R = float(promote_type(typeof(d), typeof(zero_mode), typeof(slope), typeof(offset)))
+    # d ≈ 0 : R(zero_mode), R(exp(offset + slope * log(d)))
+    ifelse(d ≈ 0, promote(std(zero_mode_dist), exp(offset + slope * log(d)))...)
+end
+
+
+rng = Random.default_rng()
+
+trafo_dht = FFTW.plan_r2r(zeros(2 .* dims), FFTW.DHT)
+
+D = dist_array(2 .* dims, harmonic_pad_distances)
+
+x = collect(distances[1]:distances[1]:distances[1] * dims[1];)
+x_pad = collect(distances[1]:distances[1]:distances[1] * dims[1]*2;)
+
 
 function gaussian_shape(p::NamedTuple{(:α , :μ, :σ)}, x::Real)
     p.α.* pdf(Normal(p.μ,p.σ),x)
 end
 
 prior = NamedTupleDist(
-    α = Uniform(0.05, 0.5),
-    μ = Uniform(1e-3, 1),
-    σ = Uniform(1e-3, 0.5)
+    α = Uniform(1e-15, 0.5),
+    μ = Uniform(0.1, 0.9),
+    σ = Uniform(1e-15, 0.05)
 )
 
 truth = rand(prior)
 
 truth = (
-    α = 0.3,
-    μ = 0.5,
-    σ = 0.02
+    α = 0.4,
+    μ = 0.3,
+    σ = 0.01
 )
 
-# Why not simply true_shape = gaussian_shape.(truth, x) oder so
-true_shape = map(xx -> gaussian_shape(truth, xx),x) # i.e. [f(elem) for elem in x] with function f(xx) = gaussian_shape(truth, xx)
+true_shape = map(xx -> gaussian_shape(truth, xx),x)
 
 
-MvNormal(0,1)
+noise_mean = zeros(size(x_pad))
+N = fill(1. / harmonic_pad_distances[1], size(x_pad))
 
-noise_mean = zeros(size(x))
-N = fill(1, size(x))
+mask = collect(1:length(x_pad))
+mask = mask .<= length(mask)÷2
+
+function adjoint_mask(x)
+    y = zeros(size(mask))
+    y[mask] = x
+    return y
+end
+
+function adjoint_mask(dp::Vector{ForwardDiff.Dual{T, V, N}}) where {T,V,N}
+    val_res = adjoint_mask(ForwardDiff.value.(dp))
+    psize = size(ForwardDiff.partials(dp[1]), 1)
+    ps = x -> ForwardDiff.partials.(dp, x)
+    val_ps = map((x -> adjoint_mask(ps(x))), 1:psize)
+    ForwardDiff.Dual{T}.(val_res, val_ps...)
+end
+
 # n = map(NN -> rand(Normal(0,NN)),N)
 
-# Draw from Mulitvariate Normal Distribution with mean 0, std 1
-n_h = rand(MvNormal(noise_mean,N)) #shouldnt N have to be a correlation matrix here!?
+n_h = rand(MvNormal(noise_mean,N))
 
-k = collect(0:(length(x))÷2 -1);
 
-# Spits out one array with declining vals from 1e7 and ascending again to 1e7
+
 function kernel()
-    pspec = 1000 ./(0.0001.+(k/0.1).^4)
-    positive_modes = pspec
-    negative_modes = positive_modes[end:-1:1]
-    [positive_modes; negative_modes]
+    corr = 1e20 ./(1 .+ (D/0.005).^6)
+    corr[1] = 1000
+    corr
 end;
 
 corr = kernel()
 
-# FFT plan. To FFT an array write ht * array
-ht = FFTW.plan_r2r(zeros(length(x)), FFTW.DHT);
+ht = FFTW.plan_r2r(zeros(length(x_pad)), FFTW.DHT) * (harmonic_pad_distances[1] / sqrt(length(x_pad)));
 ξ_b = rand(Normal(0,1),length(corr))
 
-b = ht * (corr.^0.5 .* ξ_b)./sqrt(length(x)) # background
-n = (ht * n_h)./sqrt(length(x)) # noise
+b = ht * (corr.^0.5 .* ξ_b)
+n = ht * n_h
 
 function my_ht(dp::Vector{Float64})
     ht * dp
@@ -72,37 +124,31 @@ function my_ht(dp::Vector{ForwardDiff.Dual{T, V, N}}) where {T,V,N}
 end
 
 
-data = true_shape +b #+ n + b
+data = true_shape + n[mask] + b[mask]
 plot(x,true_shape, label="signal", title="Setup")
 scatter!(x, data,marker=:x,color=:black, label="data" )
-plot!(x,b, label="background")
+plot!(x,b[mask], label="background")
 savefig("./data/tmp/setup.png")
 
 cov_z = corr + N.^2
 
 z_h = rand(MvNormal(noise_mean,cov_z.^0.5))
 
-z = (ht * z_h)./sqrt(length(x))
+z = (ht * z_h)
 
-truth2 = (
-    α = 0.3,
-    μ = 0.5,
-    σ = 0.2
-)
 
-μ = map(xx -> gaussian_shape(truth2, xx),x)
-residual = data #.- μ
-tt  = my_ht((1 ./cov_z) .* my_ht(residual))
-noise_weigted_residual = my_ht(((1 ./cov_z) .* (my_ht(residual))))./length(x)
+a = rand(length(x))
+b = rand(length(x_pad))
 
-plot(x, tt)
 
+aa = my_ht(adjoint_mask(a))
+bb = my_ht(b)[mask]
 
 likelihood = let data = data, f = gaussian_shape, N = N, x = x
     params -> begin
-        μ = map(xx -> f(params, xx),x) # gives sth that looks like the signal when params==truth
+        μ = map(xx -> f(params, xx),x)
         residual = data .- μ
-        noise_weigted_residual = my_ht(((1 ./cov_z) .* (my_ht(residual))))./length(x)  #FIXME weights
+        noise_weigted_residual = my_ht(((1 ./cov_z) .* (my_ht(adjoint_mask(residual)))))[mask]  #FIXME weights
         llhd = - 0.5 .* sum(residual .* noise_weigted_residual)
         # llhd = logpdf(MvNormal(μ,N),data)
         return LogDVal(llhd)
